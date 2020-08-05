1b8496d4a162b4a2d80258e0705e8c12d728c13b
Phodal Huang <h@phodal.com>
2020-08-05 15:10:28 +0800

    feat: use parse for xml

--- src/arsc/binary_resource_file.rs
+++ src/arsc/binary_resource_file.rs
@@ -1,9 +1,11 @@
 use std::io::{Cursor, Seek};
 use byteorder::{LittleEndian, ReadBytesExt};
-use abxml::visitor::{ModelVisitor, XmlVisitor, Executor};
+use abxml::visitor::{ModelVisitor, XmlVisitor, Executor, ChunkVisitor, Origin, Resources};
 use abxml::decoder::Decoder;
 use abxml::chunks::{ChunkLoaderStream, Chunk};
 use failure::{ResultExt, Error};
+use std::borrow::Borrow;
+use abxml::model::Element;
 
 pub struct BinaryResourceFile {}
 
@@ -30,22 +32,37 @@ impl BinaryResourceFile {
         cursor.set_position(u64::from(header_size));
         let stream = ChunkLoaderStream::new(cursor);
 
+        let resources = Resources { packages: Default::default(), main_package: None };
+        let mut visitor = XmlVisitor::new(resources.borrow());
         for c in stream {
-            match c.context("error reading next chunk")? {
-                Chunk::StringTable(_) => {}
-                Chunk::Package(_) => {}
-                Chunk::TableTypeSpec(_) => {}
-                Chunk::TableType(_) => {}
-                Chunk::XmlNamespaceStart(_) => {}
-                Chunk::XmlNamespaceEnd(_) => {}
-                Chunk::XmlTagStart(_) => {}
-                Chunk::XmlTagEnd(_) => {}
-                Chunk::XmlText(_) => {}
-                Chunk::Resource(_) => {}
-                Chunk::Unknown => {}
+            let chunk = c.context("error reading next chunk")?;
+            match chunk {
+                Chunk::StringTable(stw) => {
+                    visitor.visit_string_table(stw, Origin::Global);
+                }
+                Chunk::XmlNamespaceStart(xnsw) => {
+                    visitor.visit_xml_namespace_start(xnsw);
+                }
+                Chunk::XmlNamespaceEnd(xnsw) => {
+                    visitor.visit_xml_namespace_end(xnsw);
+                }
+                Chunk::XmlTagStart(xnsw) => {
+                    visitor.visit_xml_tag_start(xnsw);
+                }
+                Chunk::XmlTagEnd(xnsw) => {
+                    visitor.visit_xml_tag_end(xnsw);
+                }
+                Chunk::XmlText(xsnw) => {
+                    visitor.visit_xml_text(xsnw);
+                }
+                Chunk::Resource(rw) => {
+                    visitor.visit_resource(rw);
+                }
+                _ => (),
             };
         };
 
+        println!("{:?}", visitor.get_resources());
         Ok(())
     }
 }
