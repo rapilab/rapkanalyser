95828423c93d26fd9d3b0c12760ee34d1e8ca962
Phodal Huang <h@phodal.com>
2020-08-05 09:55:47 +0800

    refactor: rename verify logic

--- src/analyzer/apk_size_calculator.rs
+++ src/analyzer/apk_size_calculator.rs
@@ -1,5 +1,7 @@
 use std::path::PathBuf;
 use std::fs;
+use std::fs::Metadata;
+use std::io::Error;
 
 pub trait ApkSizeCalculator {
     fn get_full_apk_download_size(&self, apk: PathBuf) -> u64;
@@ -17,8 +19,10 @@ impl GzipSizeCalculator {
         GzipSizeCalculator {}
     }
 
-    pub fn verify(&self, apk: PathBuf) {
-
+    // todo: add verify apk file
+    pub fn verify(&self, apk: PathBuf) -> bool {
+        // let mut zip = zip::ZipArchive::new(reader).unwrap();
+        true
     }
 }
 
@@ -28,9 +32,18 @@ impl ApkSizeCalculator for GzipSizeCalculator {
     }
 
     fn get_full_apk_raw_size(&self, apk: PathBuf) -> u64 {
-        self.verify(apk.clone());
-        let metadata = fs::metadata(apk);
-        metadata.unwrap().len()
+        if self.verify(apk.clone()) {
+            let metadata = fs::metadata(apk);
+            return match metadata {
+                Ok(data) => {
+                    data.len()
+                },
+                Err(_) => {
+                    0
+                },
+            };
+        }
+        return 0
     }
 
     fn get_download_size_per_file(&self, apk: PathBuf) -> u64 {
